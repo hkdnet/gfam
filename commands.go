c81c32c1be3afeb6c448ee5f2fd5503264edec6a
hkdnet <hkdnet@users.noreply.github.com>
2016-03-19 22:13:54 +0900

    initial comit

---
+++ commands.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/hkdnet/gotp-manager/command"
+)
+
+var GlobalFlags = []cli.Flag{}
+
+var Commands = []cli.Command{
+	{
+		Name:   "list",
+		Usage:  "",
+		Action: command.CmdList,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "generate",
+		Usage:  "",
+		Action: command.CmdGenerate,
+		Flags:  []cli.Flag{},
+	},
+}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}
